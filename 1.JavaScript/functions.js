209f38d88ee00776979be03d27aa0b8a34ac37ca
Wesley <wesleyrg2000@gmail.com>
2018-06-29 15:11:02 -0400

    JavaScript :O isso mesmo aaah

---
+++ 1.JavaScript/functions.js
@@ -0,0 +1,32 @@
+// ##########################################################
+//      Função                  #############################
+
+const soma = function (x, y) { return x + y; };
+console.log(soma(1, 2));                // 3
+
+function soma1(x, y) { return x + y; };
+console.log(soma1(1, 3));               // 4
+
+const soma2 = (x, y) => { return x + y };
+console.log(soma2(1, 4));               // 5
+
+const num = x => { return x };          // Não necessita de parentes 1 valor
+console.log(num(6));                    //1
+
+const nothing = () => { return 7 };
+console.log(nothing());                 // 7
+
+function mult(num1) { return num2 => num1 * num2 };
+let num1 = mult(2);
+console.log(num1(4));                   // 8
+
+function round(base, expoente = 2) {
+    let result = 1;
+    for (let count = 0; count < expoente; count++) {
+        result *= base;
+    }
+    return result;
+}
+
+console.log(round(4));                   // 16
+console.log(round(4, 4));                // 256
---
+++ 1.JavaScript/string.js
@@ -0,0 +1,5 @@
+var name = "Wesley";                // Variável universal 
+console.log(typeof "a");            // string
+console.log(String(20));            // '20'
+console.log(name + " is cool");     // Wesley is cool
+console.log(`${name} = ${name}`);   // Wesley = Wesley
